f2ec2ed1800c86031840ce9298652644a1cdb123
Carolann Bonner <carolanncan@gmail.com>
2014-01-22 18:56:53 -0800

    WIP 9.15

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
-before_action :signed_in_user, only: [:edit, :update]
+  before_action :signed_in_user, only: [:edit, :update]
+  before_action :correct_user,   only: [:edit, :update]
+
   def show
     @user = User.find(params[:id])
   end
@@ -12,15 +14,14 @@ before_action :signed_in_user, only: [:edit, :update]
     @user = User.new(user_params)
     if @user.save
       sign_in @user
-     flash[:success] = "Welcome to the my First App!"
-     redirect_to @user
+      flash[:success] = "Welcome to the my First App!"
+      redirect_to @user
     else
       render 'new'
     end
   end
 
   def edit
-    @user = User.find(params[:id])
   end
 
   def update
@@ -42,7 +43,12 @@ before_action :signed_in_user, only: [:edit, :update]
 
   # Before filters
 
-    def signed_in_user
-      redirect_to signin_url, notice: "Please sign in." unless signed_in?
-    end
+  def signed_in_user
+    redirect_to signin_url, notice: "Please sign in." unless signed_in?
+  end
+
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_url) unless current_user?(@user)
+  end
 end
